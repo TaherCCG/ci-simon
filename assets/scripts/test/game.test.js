de6f55efba397ecad8a3cf403572770e2484f2aa
TaherCCG <taher.ccg@gmail.com>
2024-03-06 13:06:17 +0000

    Refactor newGame function and add showScore function

--- assets/scripts/test/game.test.js
+++ assets/scripts/test/game.test.js
@@ -2,8 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { beforeAll } = require("jest-circus");
-const { game, newGame } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 
 beforeAll(() => {
@@ -27,26 +26,30 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
-    test("choices contains current ids", () => {
+    test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
 
-describe("new game works correctly", () => {
+describe("newGame works correctly", () => {
     beforeAll(() => {
         game.score = 42;
         game.playerMoves = ["button1", "button2"];
         game.currentGame = ["button1", "button2"];
-        game.newGame();
+        document.getElementById("score").innerText = "42";
+        newGame();
+        showScore();
     });
-    test("score is 0", () => {
-        expect(game.score).toBe(0);
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
     });
-    test("currentGame is an empty array", () => {
-        expect(game.currentGame).toEqual([]);
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
-    test("playerMoves is an empty array", () => {
-        expect(game.playerMoves).toEqual([]);
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+    test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toBe(0);
     });
 });
-
